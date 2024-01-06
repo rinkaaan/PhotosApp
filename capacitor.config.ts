5e6d4f0480f3d37b24108bdeae4af134619d2ad2
Lincoln Nguyen <137611486+rinkaaan@users.noreply.github.com>
2024-01-06 02:16:20 -0800

    Add iOS platform

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from "@capacitor/cli"
 
 const config: CapacitorConfig = {
   appId: "io.ionic.starter",
-  appName: "capacitor-app",
+  appName: "Photos",
   webDir: "dist",
   server: {
     androidScheme: "https"
